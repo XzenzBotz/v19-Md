53d98840c67fe02441d5406b371f1196d65a94b8
XzenzBotz <99593673+XzenzBotz@users.noreply.github.com>
2022-02-18 18:06:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6288279268363'] // Put your number here
+global.owner = ['628988986891'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
